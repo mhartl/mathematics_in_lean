fbbf74a9a8245414a177a08d7264700865ee9dac
Michael Hartl <michael@michaelhartl.com>
2025-07-11 21:26:10 -0700

    Prove some inequalities

--- MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -109,21 +109,50 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  rw [exp_le_exp]
+  apply add_le_add_left
+  exact h₀
+
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  rw [exp_le_exp]
+  apply add_le_add_left h₀
+
+-- an alternative using `linarith`.
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  have : exp (a + d) ≤ exp (a + e) := by
+    rw [exp_le_exp]
+    linarith
+  linarith [this]    -- not yet covered in the text
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by linarith [exp_pos a]
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left
+  apply exp_le_exp.mpr h
 
 example : 0 ≤ a ^ 2 := by
   -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  apply add_le_add_left
+  -- I needed to cancel the minus signs and asked Grok for help. The result was
+  -- `neg_le_neg`, which hasn't been covered yet and the official solutions also
+  -- use a function (`sub_le_sub_left`) that is out of scope at this point.
+  -- Glad I didn't burn a lot of time trying to figure out the answer.
+  apply neg_le_neg
+  apply exp_le_exp.mpr h
+
+example (h : a ≤ b) : c - exp b ≤ c - exp a := by
+  apply sub_le_sub_left
+  exact exp_le_exp.mpr h
+
+-- ***here
 
 example : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
